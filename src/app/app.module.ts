3b9105beadceffcab99a58677ee071666eeb313e
Amin Uddin <aminuddin01697@gmail.com>
2020-08-26 17:47:27 +0530

    new start

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,11 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { PracticeComponent } from './components/practice/practice.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PracticeComponent
+
   ],
   imports: [
     BrowserModule,
